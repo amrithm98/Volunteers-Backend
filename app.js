77992a32e3d82b69924885e7423e6d9f536bd5a1
amrithm98 <amrithchallenger@gmail.com>
2017-06-12 18:17:42 +0530

    Firebase admin sdk setup... app.js setting up

---
+++ app.js
@@ -0,0 +1,50 @@
+var admin = require("firebase-admin");
+var serviceAccount = require("firebase.json");
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var helmet = require('helmet');
+var cors = require('cors');
+var token = require('./middlewares/token')
+var router = require('./routes/index');
+var config = require('./config')()
+var debug = require('debug')('app');
+var app = express();
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://v-4-volunteers.firebaseio.com"
+});
+
+app.use(cors())
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(helmet())
+app.use(token)
+app.use('/', router);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.locals.error = err;
+    // render the error page
+    res.status(err.status || 500);
+    res.json(err);
+});
+
+module.exports = app;
