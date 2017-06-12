a0b6f2b4fee3ba280459f4e2e5cc53e0a44afdeb
amrithm98 <amrithchallenger@gmail.com>
2017-06-12 20:42:36 +0530

    Api doc set

--- routes/public/index.js
+++ routes/public/index.js
@@ -1,5 +1,20 @@
 var router = require('express').Router();
 
+/**
+ * @api {get} / Simple Get Request
+ * @apiName Public Get
+ * @apiGroup Public
+ * @apiSuccessExample Success-Response:
+ *     {
+ *       "success": "true",
+ *       "api": "public"
+ *     }
+ * @apiErrorExample Error-Response:
+ *     {
+ *       "success": "false"
+ *     }
+ */
+
 router.get('/', (req, res, next) => {
     var obj = { success: true, api: "public" }
     res.send(obj).status(200);
