4d7bac9bb8622e9a2290e5b09a27d8faf766a62e
amrithm98 <amrithchallenger@gmail.com>
2017-06-18 12:53:11 +0530

    Added admin model

---
+++ models/volunteer.js
@@ -0,0 +1,56 @@
+"use strict";
+
+//id is the primary key and it is used as drishti/dhwani id to create QR codes
+
+module.exports = function(sequelize, DataTypes) {
+    var Students = sequelize.define("student", {
+        name: DataTypes.STRING,
+        uid: {
+            //user_id from google idToken
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        phone: {
+            type: DataTypes.STRING,
+            unique: true
+        },
+        email: {
+            type: DataTypes.STRING,
+            unique: true
+        },
+        accomodation: {
+            type: DataTypes.ENUM,
+            values: ['none', 'male', 'female'],
+            defaultValue: 'none'
+        },
+        status: {
+            type: DataTypes.ENUM,
+            values: ['active', 'banned', 'pending'],
+            defaultValue: 'active'
+        },
+        score: {
+            type: DataTypes.DECIMAL,
+            defaultValue: 0
+        },
+        registered: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        },
+        picture: {
+            type: DataTypes.STRING,
+            defaultValue: ''
+        }
+    }, 
+    {
+        classMethods: {
+            associate: function(models) {
+                // models.student.belongsTo(models.college)
+                // models.college.hasMany(models.student)
+            }
+        }
+
+    });
+
+    return Students;
+};
