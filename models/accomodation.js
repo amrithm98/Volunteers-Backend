eb359fda2f3a951223b9e0afdcd35b60b1323418
amrithm98 <amrithchallenger@gmail.com>
2017-06-18 14:30:29 +0530

    Models bug fixes

--- models/accomodation.js
+++ models/accomodation.js
@@ -4,13 +4,14 @@
 module.exports = function(sequelize, DataTypes) {
 
     var Accomodation = sequelize.define("accomodation", {
-
+    	
         
     }, 
         {
         classMethods: {
             associate: function(models) {
-                
+                models.accomodation.hasOne(models.event);
+        		models.event.belongsTo(models.accomodation);
             }
         }
 
--- models/admin.js
+++ models/admin.js
@@ -7,14 +7,13 @@ module.exports = function(sequelize, DataTypes) {
     var Admin = sequelize.define("admin", {
         name: DataTypes.STRING,
         uid: {
-            type: DataTypes.STRING,
+            type: DataTypes.STRING(100),
+            unique:true,
             allowNull: false,
-            unique: true,
-            primaryKey:true
-        },
+         },
         email: {
-            type: DataTypes.STRING,
-            unique: true,
+            type: DataTypes.STRING(100),
+            unique:true,
             allowNull: true
         },
         phone: {
@@ -29,25 +28,18 @@ module.exports = function(sequelize, DataTypes) {
         college:DataTypes.STRING,
         registered: {
             type: DataTypes.BOOLEAN,
-            defaultValue: false
-        },
-            /*
-            Status codes
-            ------------
-            0 - not verified
-            10 - superadmin - has all privilages
-            9 - admin - cannot add/edit other admins
-            8 - event admin - can edit their event and see details about it
-            7 - registration desk- can view registered students and corresponding events, can verify payments by scanning qr code
-            6 - hospitality - view accomodation details
-            5 - event volunteer - can check whether a student is eligible to enter an event and mark attendance
-            */
-
-    }, {
-        classMethods: {
-            associate: function(models) {}
-        }
-
+            defaultValue: false            
+            // Status codes
+            // ------------
+            // 0 - not verified
+            // 10 - superadmin - has all privilages
+            // 9 - admin - cannot add/edit other admins
+            // 8 - event admin - can edit their event and see details about it
+            // 7 - registration desk- can view registered students and corresponding events, can verify payments by scanning qr code
+            // 6 - hospitality - view accomodation details
+            // 5 - event volunteer - can check whether a student is eligible to enter an event and mark attendance
+            
+    }
     });
 
     return Admin;
