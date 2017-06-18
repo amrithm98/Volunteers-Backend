fd97eebb970d0ebcadbf61ed0b158491ed32266d
amrithm98 <amrithchallenger@gmail.com>
2017-06-18 17:10:20 +0530

    Changes

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -5,7 +5,7 @@ var admin = require("firebase-admin");
 var _ = require('underscore')
 var Promise = require('bluebird')
 var models = require("../../models");
-
+var Admin=models.admin;
 /**
  * @api {post} /volunteer-admin/auth/login Login an Admin
  * @apiName Login
@@ -27,7 +27,7 @@ var models = require("../../models");
     message: "Auth Error"
 }
  */
- 
+
 router.post('/login', function(req, res, next) {
 	console.log('login route');
     profile = req.profile;
@@ -47,7 +47,7 @@ router.post('/login', function(req, res, next) {
             email: profile.email
         });
     }).then(user => {
-        res.json(_.pick(user, 'name', 'picture', 'status'))
+        res.json(user);
     }).catch(function(error) {
         res.status(500).send({
             code: 2,
@@ -56,6 +56,45 @@ router.post('/login', function(req, res, next) {
         })
         debug(error);
     });
-})
+});
+
+/**
+ * @api {post} /student/register Register Student
+ * @apiDescription Register student by adding phone number, collegeId and accomodation request
+ * @apiGroup Student
+ * @apiParam {String} phone Phone Number
+ * @apiParam {Enum=['none','male','female']} accomodation=none Whether accomodation is needed
+ * @apiParam {id} collegeId college Id of the related college
+ *
+ * @apiExample request
+ *{
+      "phone":456987132,
+      "collegeId":2
+  }
+ *
+ * @apiSuccessExample success
+ * "registered"
+ * 
+ * @apiErrorExample error
+ * {
+      "code": 13,
+      "message": "Could not register student"
+    }
+ * @apiUse tokenErrors
+ *  
+ */
+
+router.post('/register', (req, res, next) => {
+    req.body.registered = true;
+    Admin.update(_.pick(req.body, 'phone','collegeId','registered'), {
+        where: {
+            uid: req.uid
+        }
+    }).then(result => {
+        res.json("Registered");
+    }).catch(error => {
+        res.status(400).json(constant.registerFailed);
+    });
+});
 
 module.exports = router;
