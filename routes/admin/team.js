27c0850c443d2911cfacfc34b6ebdea40e8166fe
amrithm98 <amrithchallenger@gmail.com>
2017-07-13 22:54:58 +0530

    Bug fix

--- routes/admin/team.js
+++ routes/admin/team.js
@@ -8,6 +8,7 @@ var _ = require('underscore');
 
 router.put('/:id/add', (req, res, next) => {
     var team = req.params.id;
+    var Model = "";
     switch (team) {
         case "Accomodation":
             Model = models.accomodation;
@@ -28,7 +29,7 @@ router.put('/:id/add', (req, res, next) => {
             Model = models.sponsorship;
             break;
         default:
-            return res.json({ "msg": "Incorrect Domain" });
+            return res.json({ "msg": "Incorrect Team Name" });
     }
     debug(Model);
     Model.create(req.body).then(team => {
@@ -50,5 +51,42 @@ router.put('/:id/add', (req, res, next) => {
 
 });
 
-// router.get('/')
+router.get('/:id/:eventId', (req, res, next) => {
+    var team = req.params.id;
+    var eventID = req.params.eventId;
+    var Model = "";
+    switch (team) {
+        case "Accomodation":
+            Model = models.accomodation;
+            break;
+        case "Food_and_Venue":
+            Model = models.foodVenue;
+            break;
+        case "Publicity":
+            Model = models.publicity;
+            break;
+        case "Registration":
+            Model = models.registration;
+            break;
+        case "Sessions":
+            Model = models.sessions;
+            break;
+        case "Sponsorship":
+            Model = models.sponsorship;
+            break;
+        default:
+            return res.json({ "msg": "Incorrect Team Name" });
+    }
+    Model.findAll({
+        where: {
+            eventId: eventID
+        }
+    }).then(result => {
+        return res.json(result);
+    }).catch(error => {
+        return res.status(400).json(constant.cantCreateFeed)
+    });
+
+
+});
 module.exports = router;
